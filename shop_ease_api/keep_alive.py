cb0a2147d688f98834403c01a17929bfd5a3559f
ripionck <ripion.chakmar@gmail.com>
2025-02-25 09:21:33 +0600

    added server alive function

--- shop_ease_api/keep_alive.py
+++ shop_ease_api/keep_alive.py
@@ -1,6 +1,7 @@
 import time
 import requests
-from django.conf import settings
+from datetime import datetime
+
 
 URL = "https://shop-ease-3oxf.onrender.com"
 INTERVAL = 30
@@ -9,12 +10,18 @@ INTERVAL = 30
 def ping_server():
     while True:
         try:
-            response = requests.get(URL)
-            print(f"Ping successful - Status Code: {response.status_code}")
-        except Exception as e:
-            print(f"Ping failed: {str(e)}")
+            response = requests.get(URL, timeout=10)
+            if response.status_code == 200:
+                print(
+                    f"[{datetime.now()}] Ping successful - Status Code: {response.status_code}")
+            else:
+                print(
+                    f"[{datetime.now()}] Ping failed - Status Code: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"[{datetime.now()}] Ping failed: {str(e)}")
         time.sleep(INTERVAL)
 
 
 if __name__ == "__main__":
+    print("Starting server pinger...")
     ping_server()
