5791022bb02da86814dcc3a0a65a9ee1a46cccc5
ripionck <ripion.chakmar@gmail.com>
2025-03-08 01:57:46 +0600

    added keep alive server function

--- shop_ease_api/settings.py
+++ shop_ease_api/settings.py
@@ -15,6 +15,8 @@ from datetime import timedelta
 from dotenv import load_dotenv
 from pathlib import Path
 import dj_database_url
+from .keep_alive import ping_server
+ping_server()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
